e572b43cf34ea73db166abe7eb25fff2926521a4
Syed Taseer Shah <66362477+Taseeeer@users.noreply.github.com>
2023-03-08 11:18:39 +0500

    input changes

--- src/components/products/products-filter.jsx
+++ src/components/products/products-filter.jsx
@@ -4,13 +4,13 @@ import { BiSearch } from 'react-icons/bi';
 
 export default function ProductFilter() {
 
-    const { setSearchFilter, handleSortedItems } = useContext(AppContext);
+    const { setSearchFilter, searchFilter, handleSortedItems } = useContext(AppContext);
 
     return (
         <div className='py-4 flex justify-center'>
             <div className='flex items-center gap-4 px-4 border rounded'>
                 <BiSearch className='text-[1.5rem] text-blue-600' />
-                <input placeholder='Search by name' className='w-[12.5rem] md:w-[31.5rem] rounded outline-none p-2' onChange={(e) => setSearchFilter(e.target.value)} />
+                <input placeholder='Search by name' value={searchFilter} className='w-[12.5rem] md:w-[31.5rem] rounded outline-none p-2' onChange={(e) => setSearchFilter(e.target.value)} />
             </div>
         </div>
     )
